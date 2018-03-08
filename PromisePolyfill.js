37c26b031829892158d599e57cd76cfe6b29ce40
leeseean chiu <leeseean@qq.com>
2018-03-08 22:38:04 +0800

    用原生js模拟es6的Promise实现的then功能

---
+++ PromisePolyfill.js
@@ -0,0 +1,62 @@
+/* 
+  原生js模拟promise
+*/
+const PromisePolyfill = (() => {
+    const promiseStatusSymbol = Symbol('PromiseStatus');
+    const promiseValueSymbol = Symbol('PromiseValue');
+    const STATUS = {
+        PENDING: 'PENDING',
+        FULFILLED: 'FULFILLED',
+        REJECTED: 'REJECTED'
+    };
+
+    function resolve() {
+        this[promiseValueSymbol] = arguments[0];
+        this[promiseStatusSymbol] = STATUS['FULFILLED'];
+    }
+
+    function reject() {
+        this[promiseValueSymbol] = arguments[0];
+        this[promiseStatusSymbol] = STATUS['REJECTED'];
+    }
+
+    function noop() {}
+
+    class myPromise {
+        constructor(resolver) {
+            if (typeof resolver !== 'function') {
+                throw new TypeError(`parameter 1 must be a function, but get a ${typeof func}`);
+            }
+            this[promiseStatusSymbol] = STATUS['PENDING'];
+            resolver(
+                resolve.bind(this),
+                reject.bind(this)
+            );
+        }
+        then(callback) {
+            const interval = setInterval(() => {
+                if (this[promiseStatusSymbol] === 'FULFILLED' || this[promiseStatusSymbol] === 'REJECTED') {
+                    clearInterval(interval);
+                    callback(this[promiseValueSymbol], resolve.bind(this), reject.bind(this));
+                    this[promiseStatusSymbol] = 'PENDING';
+                }
+            });
+            return this;
+        }
+    }
+    return myPromise;
+})();
+//测试，下面会先打印出111，再打印出222，333
+new PromisePolyfill(function (resolve, reject) {
+    setTimeout(() => {
+        resolve(222);
+        console.log(111)
+    }, 1000);
+}).then(function (res, resolve, reject) {
+    setTimeout(() => {
+        resolve(333);
+        console.log(res)
+    }, 3000);
+}).then(function (res, resolve, reject) {
+    console.log(res);
+});
